cbf038bf1b3daa17a9f4274c64537a8cc052bbed
Geoffroy Van Elsuve <geoffroy-externe.van-elsuve@enedis.fr>
2026-09-03 17:17:20 +0200

    fix: failed when target file not exist
    If target file not exists, it will not be process because of count line.
    Fix by managing without issue
    
    issue: #3

--- grab_ip.sh
+++ grab_ip.sh
@@ -27,23 +27,29 @@ do
   grab_ip_print_header "📊 RAPPORT DES IPs du domain : ${domain_name}"
   ips=$(while IFS=$'\n' read -r d; 
   do
-    grab_ip_resolve_dns $d A
-    grab_ip_resolve_dns $d AAAA
+    if [ -n "$d" ]
+	then
+      grab_ip_resolve_dns $d A
+      grab_ip_resolve_dns $d AAAA
+	fi
   done < ${file})
-  
+
   # Append to file
   domain_ips_file=${target_dir}/${domain_name}_ips.txt
+  nb_line=0
   if [ -f ${domain_ips_file} ]
   then 
-  nb_line=$(wc -l < ${domain_ips_file})
-  ips="${ips}
+	nb_line=$(wc -l < ${domain_ips_file})
+	ips="${ips}
 $(cat ${domain_ips_file})"
+  fi
+  
+  
   echo "${ips}" | sort | uniq > ${domain_ips_file}
   nb_new_line=$(wc -l < ${domain_ips_file})
   if [[ ${nb_new_line} -ne ${nb_line} ]]
   then
     echo "$((nb_new_line - nb_line)) new IPs added"
   fi
-  fi
-
+  
 done
